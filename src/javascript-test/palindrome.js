e0d8b621f7565e3c5ddaa33dffefbf3b6cbc921a
Igor Eskin <eskinhd@gmail.com>
2019-06-15 13:11:01 -0400

    isPalindrome ready

--- src/javascript-test/palindrome.js
+++ src/javascript-test/palindrome.js
@@ -1,1 +1,17 @@
-/* Export your NodeJS module from this file */
+/* 
+With this try-catch syntax, JavaScript will actually generate an Error object 
+with 2 properties called name and message, so we don't have to create an Error 
+object separately. I also intentionally refrained from using the reverse() method. 
+*/
+
+const isPalindrome = (str) => {
+  try {
+    const stripped = str.replace(/[^0-9-zA-Z]/gi, '');
+    return stripped.length > 0 && stripped === stripped.split('').reduce((aggr, el) => el + aggr, '');
+  } catch (error) {
+    console.error(`${error.name}: the argument must be a string, otherwise ${error.message}!`)
+  }
+}
+
+
+module.exports = isPalindrome;
